79fccc6eb3499b28df7be9beabf77ec48a25a720
georgy_minaev <george06mi@gmail.com>
2024-06-30 17:20:50 +0300

    Security

---
+++ src/main/java/com/example/judoStore/configuration/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.judoStore.configuration;
+
+import com.example.judoStore.service.impl.BasicAuthenticationManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   BasicAuthenticationManager authenticationManager) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().authenticated())
+                .authenticationManager(authenticationManager)
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
